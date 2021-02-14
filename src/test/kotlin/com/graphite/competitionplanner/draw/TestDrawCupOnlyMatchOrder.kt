c5e181e6fb4fc1c46a9295064abac82a45f4a7f2
Truls Haraldsson <truls.haraldsson@gmail.com>
2021-02-14 21:42:16 +0100

    Adding test for match order

---
+++ src/test/kotlin/com/graphite/competitionplanner/draw/TestDrawCupOnlyMatchOrder.kt
@@ -0,0 +1,172 @@
+package com.graphite.competitionplanner.draw
+
+import com.graphite.competitionplanner.api.*
+import com.graphite.competitionplanner.api.competition.CategoryMetadataSpec
+import com.graphite.competitionplanner.api.competition.CompetitionCategoryApi
+import com.graphite.competitionplanner.repositories.PlayerRepository
+import com.graphite.competitionplanner.repositories.RegistrationRepository
+import com.graphite.competitionplanner.service.*
+import com.graphite.competitionplanner.service.competition.CompetitionCategoryService
+import com.graphite.competitionplanner.service.competition.DrawService
+import org.junit.jupiter.api.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.LocalDate
+import java.time.LocalDateTime
+import kotlin.random.Random
+
+@SpringBootTest
+class TestDrawCupOnlyMatchOrder(
+    @Autowired val playerRepository: PlayerRepository,
+    @Autowired val registrationService: RegistrationService,
+    @Autowired val matchService: MatchService,
+    @Autowired val competitionCategoryService: CompetitionCategoryService,
+    @Autowired val registrationRepository: RegistrationRepository,
+    @Autowired val drawService: DrawService,
+    @Autowired val categoryService: CategoryService,
+    @Autowired val clubApi: ClubApi,
+    @Autowired val competitionApi: CompetitionApi,
+    @Autowired val playerApi: PlayerApi,
+    @Autowired val competitionCategoryApi: CompetitionCategoryApi,
+    @Autowired val categoryApi: CategoryApi
+) {
+    lateinit var club: ClubDTO
+    lateinit var competition: CompetitionDTO
+    lateinit var competitionCategory: CompetitionCategoryDTO
+    var players = mutableListOf<PlayerDTO>()
+
+    @BeforeEach
+    fun setUp(){
+        club = createClub()
+        competition = setupCompetitionFor(club)
+        competitionCategory = addCompetitionCategoryTo(competition, "Herrar 1")
+        setModeToCupOnlyFor(competitionCategory)
+
+        for (i in 1..8) {
+            val player = addPlayer("Player$i", club)
+            setSingleRankOn(player, 100 - i*10)
+            registerPlayerTo(player, competitionCategory)
+            players.add(player)
+        }
+
+        drawService.createDraw(competitionCategory.competitionCategoryId)
+    }
+
+    private fun addCompetitionCategoryTo(competition: CompetitionDTO, categoryName: String) : CompetitionCategoryDTO{
+        val categories = categoryApi.getCategories()
+        val category = categories.filter { it.categoryName == categoryName }[0]
+        return competitionCategoryApi.addCategoryToCompetition(
+            competition.id,
+            category.categoryId
+        ).categories[0]
+    }
+
+    private fun setModeToCupOnlyFor(competitionCategory: CompetitionCategoryDTO) {
+        val categoryMetadata = categoryService.getCategoryMetadata(competitionCategory.competitionCategoryId)
+        val categoryMetadataSpec = CategoryMetadataSpec(
+            cost = categoryMetadata.cost,
+            startTime = categoryMetadata.startTime,
+            drawTypeId = 2, // CUP ONLY. How is it set in database though?
+            nrPlayersPerGroup = categoryMetadata.nrPlayersPerGroup,
+            nrPlayersToPlayoff = categoryMetadata.nrPlayersToPlayoff,
+            poolDrawStrategyId = categoryMetadata.poolDrawStrategyId
+        )
+
+        categoryService.updateCategoryMetadata(
+            competitionCategory.competitionCategoryId,
+            categoryMetadata.id,
+            categoryMetadataSpec
+        )
+    }
+
+    private fun registerPlayerTo(player: PlayerDTO, competitionCategory: CompetitionCategoryDTO) {
+        registrationService.registerPlayerSingles(
+            RegistrationSinglesDTO(
+                null, player.id,
+                competitionCategory.competitionCategoryId
+            )
+        )
+    }
+
+    private fun setSingleRankOn(player: PlayerDTO, value: Int) {
+        playerRepository.addPlayerRanking(player.id, value, "SINGLES")
+    }
+
+    private fun createClub(): ClubDTO {
+        return clubApi.addClub(
+            ClubDTO(
+                null,
+                "TestClub" + Random.nextLong().toString(),
+                "Testroad 12B"
+            )
+        )
+    }
+
+    private fun addPlayer(firstName: String, club: ClubDTO) : PlayerDTO{
+        return playerApi.addPlayer(
+            PlayerSpec(
+                firstName,
+                "Testsson",
+                ClubNoAddressDTO(
+                    club.id!!,
+                    club.name
+                ),
+                LocalDate.of(2000, 3, 3)
+            )
+        )
+    }
+
+    private fun setupCompetitionFor(clubId: ClubDTO): CompetitionDTO {
+        return competitionApi.addCompetition(
+            CompetitionSpec(
+                "LocationA",
+                "Welcome to competition A",
+                clubId.id!!,
+                LocalDate.now(),
+                LocalDate.now()
+            )
+        )
+    }
+
+    @AfterEach
+    fun removeMatchesAndRegistrations() {
+        // Remove matches
+        matchService.deleteMatchesInCategory(competition.id)
+
+        // Remove registrations and delete category
+        val registrationIds = registrationRepository.getRegistrationIdsInCategory(competition.id)
+        for (id in registrationIds) {
+            registrationService.unregister(id)
+        }
+        competitionCategoryService.deleteCategoryInCompetition(competition.id)
+
+    }
+
+    @Test
+    fun thenBestAndWorstRankedPlayerShouldMeetInFirstMatch(){
+        val matches = matchService.getMatchesInCategory(competitionCategory.competitionCategoryId)
+
+        val firstMatch = matches.filter { it.matchOrderNumber == 1 }[0]
+        val bestPlayer = players[0]
+        val worstPlayer = players[7]
+
+        Assertions.assertEquals(bestPlayer.id, firstMatch.firstPlayer[0].id,
+            "Expected to find the best ranked player in the first match")
+        Assertions.assertEquals(worstPlayer.id, firstMatch.secondPlayer[0].id,
+            "Expected to find the worst ranked player in the first match.")
+    }
+
+    @Test
+    fun thenSecondBestAndSecondWorstPlayerShouldMeetInSecondMatch(){
+        val matches = matchService.getMatchesInCategory(competitionCategory.competitionCategoryId)
+
+        val secondMatch = matches.filter { it.matchOrderNumber == 2 }[0]
+        val secondBestPlayer = players[1]
+        val secondWorstPlayer = players[6]
+
+        Assertions.assertEquals(secondBestPlayer.id, secondMatch.firstPlayer[0].id,
+            "Expected to find the second best ranked player in the second match")
+        Assertions.assertEquals(secondWorstPlayer.id, secondMatch.secondPlayer[0].id,
+            "Expected to find the second worst ranked player in the second match.")
+    }
+}
