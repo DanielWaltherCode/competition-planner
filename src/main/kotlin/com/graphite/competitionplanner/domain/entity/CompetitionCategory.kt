0389f3e857b99fc15db3bc4cf141e8b850f97a2f
Truls Haraldsson <truls.haraldsson@gmail.com>
2021-09-05 18:06:01 +0200

    Implementing update competition category use case (#51)

--- src/main/kotlin/com/graphite/competitionplanner/domain/entity/CompetitionCategory.kt
+++ src/main/kotlin/com/graphite/competitionplanner/domain/entity/CompetitionCategory.kt
@@ -1,8 +1,17 @@
 package com.graphite.competitionplanner.domain.entity
 
+import com.graphite.competitionplanner.domain.dto.CompetitionCategoryDTO
+
 data class CompetitionCategory(
     val id: Int,
     val category: Category,
     val settings: GeneralSettings,
     val gameSettings: GameSettings
-)
+) {
+    constructor(dto: CompetitionCategoryDTO) : this(
+        dto.id,
+        Category(dto.category),
+        GeneralSettings(dto.settings),
+        GameSettings(dto.gameSettings)
+    )
+}
