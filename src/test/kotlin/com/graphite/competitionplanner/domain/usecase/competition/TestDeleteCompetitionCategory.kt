23dfeaf8fbc0247d5a7a85a0f1e212a2b2ea8bb7
Truls Haraldsson <truls.haraldsson@gmail.com>
2021-09-05 13:35:41 +0200

    Implementing use case to delete competition category (#50)

---
+++ src/test/kotlin/com/graphite/competitionplanner/domain/usecase/competition/TestDeleteCompetitionCategory.kt
@@ -0,0 +1,34 @@
+package com.graphite.competitionplanner.domain.usecase.competition
+
+import com.graphite.competitionplanner.domain.interfaces.ICompetitionCategoryRepository
+import com.graphite.competitionplanner.domain.interfaces.NotFoundException
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.*
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestDeleteCompetitionCategory {
+
+    private final val mockedRepository = mock(ICompetitionCategoryRepository::class.java)
+    val deleteCompetitionCategory = DeleteCompetitionCategory(mockedRepository)
+
+    @Test
+    fun shouldCallRepositoryToDeleteOnce() {
+        val competitionCategoryId = 13
+        deleteCompetitionCategory.execute(competitionCategoryId)
+
+        verify(mockedRepository, times(1)).delete(competitionCategoryId)
+        verify(mockedRepository, times(1)).delete(anyInt())
+    }
+
+    @Test
+    fun repositoryExceptionShouldPropagate() {
+        val competitionCategoryId = 1337
+        `when`(mockedRepository.delete(competitionCategoryId)).thenThrow(NotFoundException::class.java)
+
+        Assertions.assertThrows(NotFoundException::class.java) {
+            deleteCompetitionCategory.execute(competitionCategoryId)
+        }
+    }
+}
