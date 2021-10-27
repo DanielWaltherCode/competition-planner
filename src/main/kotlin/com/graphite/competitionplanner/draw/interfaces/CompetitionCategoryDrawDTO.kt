2215cc8fb3ed736a43f5f8522c20a5e92b667fc0
Truls Haraldsson <truls.haraldsson@gmail.com>
2021-10-27 21:34:41 +0200

    Fetching the competition draw dto from repository
    Only playoff matches fetched at the moment

---
+++ src/main/kotlin/com/graphite/competitionplanner/draw/interfaces/CompetitionCategoryDrawDTO.kt
@@ -0,0 +1,79 @@
+package com.graphite.competitionplanner.draw.interfaces
+
+import com.graphite.competitionplanner.domain.entity.Round
+import com.graphite.competitionplanner.player.interfaces.PlayerWithClubDTO
+
+/**
+ * A description of all matches in a competition category
+ */
+data class CompetitionCategoryDrawDTO(
+    /**
+     * Id of the competition category
+     */
+    val competitionCategoryId: Int,
+    /**
+     * List of matches played in play off
+     */
+    val playOff: List<PlayOffMatchDTO>,
+    /**
+     * List of groups including players and matches
+     */
+    val groupDraw: List<GroupDrawDTO>
+)
+
+/**
+ * A match played in play off
+ */
+data class PlayOffMatchDTO(
+    /**
+     * List of players in team one. Contains two players if it is a double game
+     */
+    val player1: List<PlayerWithClubDTO>,
+    /**
+     * List of players in team two. Contains two players if it is a double game
+     */
+    val player2: List<PlayerWithClubDTO>,
+    /**
+     * Match order. Starts at 1 and ends at the number equal to the total matches for this round.
+     *
+     * Important: Winners from matches with order 1 and 2, end up playing each other i the next round. The same goes
+     * for 3 and 4, 5 and 6, etc.
+     */
+    var order: Int,
+    /**
+     * The round this match is played in.
+     */
+    var round: Round
+)
+
+/**
+ * A description of a group, including name, players, and matches.
+ */
+data class GroupDrawDTO(
+    /**
+     * Name of the group
+     */
+    val name: String,
+    /**
+     * List of players in this group
+     */
+    val players: List<PlayerWithClubDTO>,
+    /**
+     * Matches in this group
+     */
+    val matches: List<GroupMatchDTO>
+)
+
+/**
+ * A match player in the group stage
+ */
+data class GroupMatchDTO(
+    /**
+     * List of players in team one. Contains two players if it is a double game
+     */
+    val player1: List<PlayerWithClubDTO>,
+    /**
+     * List of players in team two. Contains two players if it is a double game
+     */
+    val player2: List<PlayerWithClubDTO>
+)
--- src/main/kotlin/com/graphite/competitionplanner/draw/repository/CompetitionDrawRepository.kt
+++ src/main/kotlin/com/graphite/competitionplanner/draw/repository/CompetitionDrawRepository.kt
@@ -1,15 +1,24 @@
 package com.graphite.competitionplanner.draw.repository
 
-import com.graphite.competitionplanner.Tables.MATCH
-import com.graphite.competitionplanner.Tables.POOL_DRAW
+import com.graphite.competitionplanner.Tables.*
+import com.graphite.competitionplanner.club.interfaces.ClubDTO
+import com.graphite.competitionplanner.domain.entity.Round
 import com.graphite.competitionplanner.draw.domain.*
+import com.graphite.competitionplanner.draw.interfaces.CompetitionCategoryDrawDTO
+import com.graphite.competitionplanner.draw.interfaces.GroupDrawDTO
 import com.graphite.competitionplanner.draw.interfaces.ICompetitionDrawRepository
+import com.graphite.competitionplanner.draw.interfaces.PlayOffMatchDTO
 import com.graphite.competitionplanner.draw.service.MatchType
 import com.graphite.competitionplanner.draw.service.PoolDrawHelper
+import com.graphite.competitionplanner.player.interfaces.PlayerWithClubDTO
 import com.graphite.competitionplanner.tables.records.MatchRecord
 import com.graphite.competitionplanner.tables.records.PoolDrawRecord
 import org.jooq.DSLContext
+import org.jooq.Record12
+import org.jooq.RecordMapper
+import org.jooq.impl.DSL.inline
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
 
 @Repository
 class CompetitionDrawRepository(val dslContext: DSLContext) : ICompetitionDrawRepository {
@@ -43,12 +52,160 @@ class CompetitionDrawRepository(val dslContext: DSLContext) : ICompetitionDrawRe
         }
     }
 
+    override fun get(competitionCategoryId: Int): CompetitionCategoryDrawDTO {
+        val dataRows = getDataRows(competitionCategoryId)
+        val playOffMatches = constructPlayOffMatches(dataRows.filter { it.groupOrRound.isRound() })
+        val groupMatches = constructGroupDraws(dataRows.filter { !it.groupOrRound.isRound() })
+
+        return CompetitionCategoryDrawDTO(
+            competitionCategoryId,
+            playOffMatches,
+            groupMatches
+        )
+    }
+
+    private fun String.isRound(): Boolean {
+        return Round.values().any { it.name == this }
+    }
+
+    private fun constructPlayOffMatches(dataRows: List<DataRow>): List<PlayOffMatchDTO> {
+        return if (dataRows.isEmpty()) {
+            emptyList()
+        } else {
+            val matchId = dataRows.first().matchId
+            val rows = dataRows.filter { it.matchId == matchId }
+            val player1 =
+                rows.filter { it.registrationOrder == RegistrationOrder.First }.map { it.toPlayerWithClubDto() }
+            val player2 =
+                rows.filter { it.registrationOrder == RegistrationOrder.Second }.map { it.toPlayerWithClubDto() }
+            val order = rows.first().matchOrder
+            val round = Round.valueOf(rows.first().groupOrRound)
+            listOf(PlayOffMatchDTO(player1,
+                player2,
+                order,
+                round)) + constructPlayOffMatches(dataRows.drop(rows.count()))
+        }
+    }
+
+    private fun DataRow.toPlayerWithClubDto(): PlayerWithClubDTO {
+        return PlayerWithClubDTO(
+            this.playerId,
+            this.firstName,
+            this.lastName,
+            ClubDTO(
+                this.clubId,
+                this.clubName,
+                this.clubAddress
+            ),
+            this.dateOfBirth
+        )
+    }
+
+    private fun constructGroupDraws(dataRows: List<DataRow>): List<GroupDrawDTO> {
+        // TODO: Implement
+        return emptyList()
+    }
+
+    private fun getDataRows(competitionCategoryId: Int): List<DataRow> {
+        val r1 = REGISTRATION.`as`("r1")
+        val pr1 = PLAYER_REGISTRATION.`as`("pr1")
+        val p1 = PLAYER.`as`("p1")
+        val c1 = CLUB.`as`("c1")
+
+        val r2 = REGISTRATION.`as`("r2")
+        val pr2 = PLAYER_REGISTRATION.`as`("pr2")
+        val p2 = PLAYER.`as`("p2")
+        val c2 = CLUB.`as`("c2")
+
+        val m = MATCH.`as`("m")
+
+        return dslContext
+            .select(
+                m.ID.`as`("match_id"),
+                m.GROUP_OR_ROUND,
+                m.MATCH_ORDER_NUMBER,
+                m.FIRST_REGISTRATION_ID.`as`("registration_id"),
+                inline(RegistrationOrder.First.name).`as`("registration_order"), // Keeping track from which select
+                p1.ID.`as`("player_id"),
+                p1.FIRST_NAME,
+                p1.LAST_NAME,
+                p1.DATE_OF_BIRTH,
+                c1.ID.`as`("club_id"),
+                c1.NAME,
+                c1.ADDRESS
+            )
+            .from(m)
+            .join(r1).on(r1.ID.eq(m.FIRST_REGISTRATION_ID))
+            .join(pr1).on(pr1.REGISTRATION_ID.eq(r1.ID))
+            .join(p1).on(p1.ID.eq(pr1.PLAYER_ID))
+            .join(c1).on(c1.ID.eq(p1.CLUB_ID))
+            .where(m.COMPETITION_CATEGORY_ID.eq(competitionCategoryId))
+            .union(
+                dslContext
+                    .select(
+                        m.ID.`as`("match_id"),
+                        m.GROUP_OR_ROUND,
+                        m.MATCH_ORDER_NUMBER,
+                        m.SECOND_REGISTRATION_ID.`as`("registration_id"),
+                        inline(RegistrationOrder.Second.name).`as`("registration_order"), // Keeping track from which select
+                        p2.ID.`as`("player_id"),
+                        p2.FIRST_NAME,
+                        p2.LAST_NAME,
+                        p2.DATE_OF_BIRTH,
+                        c2.ID.`as`("club_id"),
+                        c2.NAME,
+                        c2.ADDRESS
+                    )
+                    .from(m)
+                    .join(r2).on(r2.ID.eq(m.SECOND_REGISTRATION_ID))
+                    .join(pr2).on(pr2.REGISTRATION_ID.eq(r2.ID))
+                    .join(p2).on(p2.ID.eq(pr2.PLAYER_ID))
+                    .join(c2).on(c2.ID.eq(p2.CLUB_ID))
+                    .where(m.COMPETITION_CATEGORY_ID.eq(competitionCategoryId))
+            ).orderBy(m.ID.`as`("match_id").desc())
+            .fetch(MyMapper())
+    }
+
+    class MyMapper :
+        RecordMapper<Record12<Int, String, Int, Int, String, Int, String, String, LocalDate, Int, String, String>, DataRow> {
+        override fun map(p0: Record12<Int, String, Int, Int, String, Int, String, String, LocalDate, Int, String, String>?): DataRow {
+            return DataRow(
+                p0?.get("match_id", MATCH.ID.type)!!,
+                p0.getValue(MATCH.GROUP_OR_ROUND)!!,
+                p0.getValue(MATCH.MATCH_ORDER_NUMBER)!!,
+                p0.getValue("registration_id", REGISTRATION.ID.type),
+                RegistrationOrder.valueOf(p0.getValue("registration_order", String::class.java)),
+                p0.getValue("player_id", PLAYER.ID.type)!!,
+                p0.getValue(PLAYER.FIRST_NAME)!!,
+                p0.getValue(PLAYER.LAST_NAME)!!,
+                p0.getValue(PLAYER.DATE_OF_BIRTH),
+                p0.getValue("club_id", CLUB.ID.type)!!,
+                p0.getValue(CLUB.NAME)!!,
+                p0.getValue(CLUB.ADDRESS)!!
+            )
+        }
+    }
+
+    class DataRow(
+        val matchId: Int,
+        val groupOrRound: String,
+        val matchOrder: Int,
+        val registrationNumber: Int,
+        val registrationOrder: RegistrationOrder, // Indicates whether this row came from first or second select i.e. first_registration or second_registration
+        val playerId: Int,
+        val firstName: String,
+        val lastName: String,
+        val dateOfBirth: LocalDate,
+        val clubId: Int,
+        val clubName: String,
+        val clubAddress: String
+    )
+
+    enum class RegistrationOrder { First, Second }
+
     private fun storePlayoff(draw: CompetitionCategoryPlayOffDrawSpec) {
         val records = draw.matches.map { it.toRecord(draw.competitionCategoryId) }
         dslContext.batchInsert(records).execute()
-
-        // TODO: Create a CompetitionCategoryDrawDTO that is returned
-        var matches = dslContext.selectFrom(MATCH).where(MATCH.COMPETITION_CATEGORY_ID.eq(draw.competitionCategoryId))
     }
 
     private fun PlayOffMatch.toRecord(competitionCategoryId: Int): MatchRecord {
