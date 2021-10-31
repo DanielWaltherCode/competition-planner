844359958cbbf4a4f9254fb850ff404c3c152645
Truls Haraldsson <truls.haraldsson@gmail.com>
2021-10-31 14:34:27 +0100

    Adding a new method in the domain to delete an existing draw

---
+++ src/test/kotlin/com/graphite/competitionplanner/draw/domain/TestDeleteDraw.kt
@@ -0,0 +1,44 @@
+package com.graphite.competitionplanner.draw.domain
+
+import com.graphite.competitionplanner.competitioncategory.interfaces.ICompetitionCategoryRepository
+import com.graphite.competitionplanner.draw.interfaces.ICompetitionDrawRepository
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.anyInt
+import org.mockito.Mockito.times
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestDeleteDraw {
+
+    private final val mockedDrawRepository = Mockito.mock(ICompetitionDrawRepository::class.java)
+    private final val mockedCompetitionCategoryRepository = Mockito.mock(ICompetitionCategoryRepository::class.java)
+
+    private val deleteDraw = DeleteDraw(mockedDrawRepository, mockedCompetitionCategoryRepository)
+
+    @Test
+    fun shouldValidateThatCompetitionCategoryExist() {
+        // Setup
+        val competitionCategoryId = 32211
+
+        // Act
+        deleteDraw.execute(competitionCategoryId)
+
+        // Assert
+        Mockito.verify(mockedCompetitionCategoryRepository, times(1)).get(competitionCategoryId)
+        Mockito.verify(mockedCompetitionCategoryRepository, times(1)).get(anyInt())
+    }
+
+    @Test
+    fun shouldDelegateToRepository() {
+        // Setup
+        val competitionCategoryId = 55911
+
+        // Act
+        deleteDraw.execute(competitionCategoryId)
+
+        // Assert
+        Mockito.verify(mockedDrawRepository, times(1)).delete(competitionCategoryId)
+        Mockito.verify(mockedDrawRepository, times(1)).delete(anyInt())
+    }
+}
