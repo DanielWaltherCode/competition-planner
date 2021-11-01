c2b947691b90b6b7c8fb37a809ea3ea1dea57572
Truls Haraldsson <truls.haraldsson@gmail.com>
2021-11-01 20:54:34 +0100

    Not initiating test data

--- src/main/kotlin/com/graphite/competitionplanner/util/SetupTestData.kt
+++ src/main/kotlin/com/graphite/competitionplanner/util/SetupTestData.kt
@@ -54,7 +54,7 @@ class EventListener(
     @EventListener
     fun onApplicationEvent(event: ContextRefreshedEvent) {
         clearTables()
-        setUpData()
+//        setUpData()
     }
 
     fun setUpData() {
