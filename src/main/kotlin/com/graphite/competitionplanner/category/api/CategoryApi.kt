2c70db4b2fa7162c2f29c549c27602e8f89346b5
Truls Haraldsson <truls.haraldsson@gmail.com>
2021-12-09 19:06:35 +0100

    Removing redundant category service

--- src/main/kotlin/com/graphite/competitionplanner/category/api/CategoryApi.kt
+++ src/main/kotlin/com/graphite/competitionplanner/category/api/CategoryApi.kt
@@ -1,7 +1,7 @@
 package com.graphite.competitionplanner.category.api
 
+import com.graphite.competitionplanner.category.domain.GetCategories
 import com.graphite.competitionplanner.category.interfaces.CategoryDTO
-import com.graphite.competitionplanner.category.service.CategoryService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("category")
 class CategoryApi(
-    val categoryService: CategoryService
+    val getCategories: GetCategories
 ) {
 
     @GetMapping
     fun getCategories(): List<CategoryDTO> {
-        return categoryService.getCategories()
+        return getCategories.execute()
     }
 
 }
--- src/test/kotlin/com/graphite/competitionplanner/category/api/TestCategoryApi.kt
+++ src/test/kotlin/com/graphite/competitionplanner/category/api/TestCategoryApi.kt
@@ -1,6 +1,6 @@
 package com.graphite.competitionplanner.category.api
 
-import com.graphite.competitionplanner.category.service.CategoryService
+import com.graphite.competitionplanner.category.domain.GetCategories
 import com.graphite.competitionplanner.util.DataGenerator
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -10,23 +10,22 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class TestCategoryApi {
 
-    private final val service = mock(CategoryService::class.java)
-    private final val api = CategoryApi(service)
+    private val mockedGetCategories = mock(GetCategories::class.java)
+    private val api = CategoryApi(mockedGetCategories)
     val dataGenerator = DataGenerator()
 
     @Test
     fun shouldDelegateToService() {
         // Setup
         val expectedCategories =
-
             listOf(dataGenerator.newCategoryDTO(name = "Class1"), dataGenerator.newCategoryDTO(name = "class2"))
-        `when`(service.getCategories()).thenReturn(expectedCategories)
+        `when`(mockedGetCategories.execute()).thenReturn(expectedCategories)
 
         // Act
         val actual = api.getCategories()
 
         // Assert
         Assertions.assertEquals(expectedCategories, actual)
-        verify(service, times(1)).getCategories()
+        verify(mockedGetCategories, times(1)).execute()
     }
 }
