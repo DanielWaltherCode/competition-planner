f7968442abd0d71500e9ddd233b3ed3e3dda72bf
Truls Haraldsson <truls.haraldsson@gmail.com>
2022-02-06 21:58:05 +0100

    Moving util function

--- src/main/kotlin/com/graphite/competitionplanner/draw/interfaces/Round.kt
+++ src/main/kotlin/com/graphite/competitionplanner/draw/interfaces/Round.kt
@@ -4,3 +4,7 @@ package com.graphite.competitionplanner.draw.interfaces
 enum class Round {
     FINAL, SEMI_FINAL, QUARTER_FINAL, ROUND_OF_16, ROUND_OF_32, ROUND_OF_64, ROUND_OF_128, UNKNOWN
 }
+
+fun String.isRound(): Boolean {
+    return Round.values().any { it.name == this }
+}
