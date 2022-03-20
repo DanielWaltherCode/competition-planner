501447ccb5c82aa684a5a09765a220eaf26f77c9
Truls Haraldsson <truls.haraldsson@gmail.com>
2022-03-20 18:10:56 +0100

    Cleaning up test cases to reduce code duplication and extensibility

--- src/test/kotlin/com/graphite/competitionplanner/draw/domain/TestPoolAndCupDrawPolicyPlacementOfSeededInFirstRound.kt
+++ src/test/kotlin/com/graphite/competitionplanner/draw/domain/TestPoolAndCupDrawPolicyPlacementOfSeededInFirstRound.kt
@@ -17,181 +17,117 @@ class TestPoolAndCupDrawPolicyPlacementOfSeededInFirstRound {
 
     data class TestData(
         val playersPerGroup: Int,
+        val playersToPlayoff: Int,
         val numberOfRegistrations: Int,
         val startRound: Round,
         val expectedFirstRoundOfMatches: List<ExpectedFirstRoundMatch>
     )
 
     data class ExpectedFirstRoundMatch(
+        /**
+         * Match order number
+         */
         val order: Int,
+        /**
+         * Possible players in position 1
+         */
         val possibleRegistrationOne: List<String>,
+        /**
+         * Possible players in position 2
+         */
         val possibleRegistrationTwo: List<String>
     )
 
+    /**
+     * Pair<Pools, PlayersToPlayOff> to Expected matches in first round
+     */
+    private val expectedMatchMap = mapOf(
+        Pair(2, 2) to listOf(
+            ExpectedFirstRoundMatch(1, listOf("A1"), listOf("B2")),
+            ExpectedFirstRoundMatch(2, listOf("A2"), listOf("B1"))
+        ),
+        Pair(3, 2) to listOf(
+            ExpectedFirstRoundMatch(1, listOf("A1"), listOf("BYE")),
+            ExpectedFirstRoundMatch(2, listOf("B2", "C1", "C2"), listOf("B2", "C1", "C2")),
+            ExpectedFirstRoundMatch(3, listOf("A2", "C1", "C2"), listOf("A2", "C1", "C2")),
+            ExpectedFirstRoundMatch(4, listOf("BYE"), listOf("B1"))
+        ),
+        Pair(4, 2) to listOf(
+            ExpectedFirstRoundMatch(1, listOf("A1"), listOf("B2", "C2", "D2")),
+            ExpectedFirstRoundMatch(2, listOf("B2", "C2", "D2"), listOf("C1", "D1")),
+            ExpectedFirstRoundMatch(3, listOf("C1", "D1"), listOf("A2", "C2", "D2")),
+            ExpectedFirstRoundMatch(4, listOf("A2", "C2", "D2"), listOf("B1"))
+        ),
+        Pair(5, 2) to listOf(
+            ExpectedFirstRoundMatch(1, listOf("A1"), listOf("BYE")),
+            ExpectedFirstRoundMatch(
+                2,
+                listOf("E1", "B2", "C2", "D2", "E2", "BYE"),
+                listOf("E1", "B2", "C2", "D2", "E2")
+            ),
+            ExpectedFirstRoundMatch(
+                3,
+                listOf("E1", "B2", "C2", "D2", "E2"),
+                listOf("E1", "B2", "C2", "D2", "E2", "BYE")
+            ),
+            ExpectedFirstRoundMatch(4, listOf("BYE"), listOf("C1", "D1")),
+            ExpectedFirstRoundMatch(5, listOf("C1", "D1"), listOf("BYE")),
+            ExpectedFirstRoundMatch(
+                6,
+                listOf("E1", "A2", "C2", "D2", "E2", "BYE"),
+                listOf("E1", "A2", "C2", "D2", "E2")
+            ),
+            ExpectedFirstRoundMatch(
+                7,
+                listOf("E1", "A2", "C2", "D2", "E2"),
+                listOf("E1", "A2", "C2", "D2", "E2", "BYE")
+            ),
+            ExpectedFirstRoundMatch(8, listOf("BYE"), listOf("B1"))
+        )
+    )
+
     private val testDataWhenTwoAreSeeded = listOf(
-        TestData(
-            4, 6, Round.SEMI_FINAL,
-            listOf(
-                ExpectedFirstRoundMatch(1, listOf("A1"), listOf("B2")),
-                ExpectedFirstRoundMatch(2, listOf("A2"), listOf("B1"))
-            )
-        ),
-        TestData(
-            4, 7, Round.SEMI_FINAL,
-            listOf(
-                ExpectedFirstRoundMatch(1, listOf("A1"), listOf("B2")),
-                ExpectedFirstRoundMatch(2, listOf("A2"), listOf("B1"))
-            )
-        ),
-        TestData(
-            4, 8, Round.SEMI_FINAL,
-            listOf(
-                ExpectedFirstRoundMatch(1, listOf("A1"), listOf("B2")),
-                ExpectedFirstRoundMatch(2, listOf("A2"), listOf("B1"))
-            )
-        ),
-        TestData(
-            4, 9, Round.QUARTER_FINAL,
-            listOf(
-                ExpectedFirstRoundMatch(1, listOf("A1"), listOf("BYE")),
-                ExpectedFirstRoundMatch(2, listOf("B2", "C1", "C2"), listOf("B2", "C1", "C2")),
-                ExpectedFirstRoundMatch(3, listOf("A2", "C1", "C2"), listOf("A2", "C1", "C2")),
-                ExpectedFirstRoundMatch(4, listOf("BYE"), listOf("B1"))
-            )
-        ),
-        TestData(
-            4, 10, Round.QUARTER_FINAL,
-            listOf(
-                ExpectedFirstRoundMatch(1, listOf("A1"), listOf("BYE")),
-                ExpectedFirstRoundMatch(2, listOf("B2", "C1", "C2"), listOf("B2", "C1", "C2")),
-                ExpectedFirstRoundMatch(3, listOf("A2", "C1", "C2"), listOf("A2", "C1", "C2")),
-                ExpectedFirstRoundMatch(4, listOf("BYE"), listOf("B1"))
-            )
-        ),
-        TestData(
-            4, 11, Round.QUARTER_FINAL,
-            listOf(
-                ExpectedFirstRoundMatch(1, listOf("A1"), listOf("BYE")),
-                ExpectedFirstRoundMatch(2, listOf("B2", "C1", "C2"), listOf("B2", "C1", "C2")),
-                ExpectedFirstRoundMatch(3, listOf("A2", "C1", "C2"), listOf("A2", "C1", "C2")),
-                ExpectedFirstRoundMatch(4, listOf("BYE"), listOf("B1"))
-            )
-        ),
-        TestData(
-            4, 12, Round.QUARTER_FINAL,
-            listOf(
-                ExpectedFirstRoundMatch(1, listOf("A1"), listOf("BYE")),
-                ExpectedFirstRoundMatch(2, listOf("B2", "C1", "C2"), listOf("B2", "C1", "C2")),
-                ExpectedFirstRoundMatch(3, listOf("A2", "C1", "C2"), listOf("A2", "C1", "C2")),
-                ExpectedFirstRoundMatch(4, listOf("BYE"), listOf("B1"))
-            )
-        ),
-        TestData(
-            4, 13, Round.QUARTER_FINAL,
-            listOf(
-                ExpectedFirstRoundMatch(1, listOf("A1"), listOf("B2", "C2", "D2")),
-                ExpectedFirstRoundMatch(2, listOf("B2", "C2", "D2"), listOf("C1", "D1")),
-                ExpectedFirstRoundMatch(3, listOf("C1", "D1"), listOf("A2", "C2", "D2")),
-                ExpectedFirstRoundMatch(4, listOf("A2", "C2", "D2"), listOf("B1"))
-            )
-        ),
-        TestData(
-            4, 14, Round.QUARTER_FINAL,
-            listOf(
-                ExpectedFirstRoundMatch(1, listOf("A1"), listOf("B2", "C2", "D2")),
-                ExpectedFirstRoundMatch(2, listOf("B2", "C2", "D2"), listOf("C1", "D1")),
-                ExpectedFirstRoundMatch(3, listOf("C1", "D1"), listOf("A2", "C2", "D2")),
-                ExpectedFirstRoundMatch(4, listOf("A2", "C2", "D2"), listOf("B1"))
-            )
-        ),
-        TestData(
-            4, 15, Round.QUARTER_FINAL,
-            listOf(
-                ExpectedFirstRoundMatch(1, listOf("A1"), listOf("B2", "C2", "D2")),
-                ExpectedFirstRoundMatch(2, listOf("B2", "C2", "D2"), listOf("C1", "D1")),
-                ExpectedFirstRoundMatch(3, listOf("C1", "D1"), listOf("A2", "C2", "D2")),
-                ExpectedFirstRoundMatch(4, listOf("A2", "C2", "D2"), listOf("B1"))
-            )
-        ),
-        TestData(
-            4, 16, Round.QUARTER_FINAL,
-            listOf(
-                ExpectedFirstRoundMatch(1, listOf("A1"), listOf("B2", "C2", "D2")),
-                ExpectedFirstRoundMatch(2, listOf("B2", "C2", "D2"), listOf("C1", "D1")),
-                ExpectedFirstRoundMatch(3, listOf("C1", "D1"), listOf("A2", "C2", "D2")),
-                ExpectedFirstRoundMatch(4, listOf("A2", "C2", "D2"), listOf("B1"))
-            )
-        ),
-        TestData(
-            4, 17, Round.ROUND_OF_16,
-            listOf(
-                ExpectedFirstRoundMatch(1, listOf("A1"), listOf("BYE")),
-                ExpectedFirstRoundMatch(
-                    2,
-                    listOf("E1", "B2", "C2", "D2", "E2", "BYE"),
-                    listOf("E1", "B2", "C2", "D2", "E2")
-                ),
-                ExpectedFirstRoundMatch(
-                    3,
-                    listOf("E1", "B2", "C2", "D2", "E2"),
-                    listOf("E1", "B2", "C2", "D2", "E2", "BYE")
-                ),
-                ExpectedFirstRoundMatch(4, listOf("BYE"), listOf("C1", "D1")),
-                ExpectedFirstRoundMatch(5, listOf("C1", "D1"), listOf("BYE")),
-                ExpectedFirstRoundMatch(
-                    6,
-                    listOf("E1", "A2", "C2", "D2", "E2", "BYE"),
-                    listOf("E1", "A2", "C2", "D2", "E2")
-                ),
-                ExpectedFirstRoundMatch(
-                    7,
-                    listOf("E1", "A2", "C2", "D2", "E2"),
-                    listOf("E1", "A2", "C2", "D2", "E2", "BYE")
-                ),
-                ExpectedFirstRoundMatch(8, listOf("BYE"), listOf("B1"))
-            )
-        ),
-        TestData(
-            4, 18, Round.ROUND_OF_16,
-            listOf(
-                ExpectedFirstRoundMatch(1, listOf("A1"), listOf("BYE")),
-                ExpectedFirstRoundMatch(
-                    2,
-                    listOf("E1", "B2", "C2", "D2", "E2", "BYE"),
-                    listOf("E1", "B2", "C2", "D2", "E2")
-                ),
-                ExpectedFirstRoundMatch(
-                    3,
-                    listOf("E1", "B2", "C2", "D2", "E2"),
-                    listOf("E1", "B2", "C2", "D2", "E2", "BYE")
-                ),
-                ExpectedFirstRoundMatch(4, listOf("BYE"), listOf("C1", "D1")),
-                ExpectedFirstRoundMatch(5, listOf("C1", "D1"), listOf("BYE")),
-                ExpectedFirstRoundMatch(
-                    6,
-                    listOf("E1", "A2", "C2", "D2", "E2", "BYE"),
-                    listOf("E1", "A2", "C2", "D2", "E2")
-                ),
-                ExpectedFirstRoundMatch(
-                    7,
-                    listOf("E1", "A2", "C2", "D2", "E2"),
-                    listOf("E1", "A2", "C2", "D2", "E2", "BYE")
-                ),
-                ExpectedFirstRoundMatch(8, listOf("BYE"), listOf("B1"))
-            )
-        ),
+        TestData(4, 2, 6, Round.SEMI_FINAL, expectedMatchMap[Pair(2, 2)]!!),
+        TestData(4, 2, 7, Round.SEMI_FINAL, expectedMatchMap[Pair(2, 2)]!!),
+        TestData(4, 2, 8, Round.SEMI_FINAL, expectedMatchMap[Pair(2, 2)]!!),
+        TestData(4, 2, 9, Round.QUARTER_FINAL, expectedMatchMap[Pair(3, 2)]!!),
+        TestData(4, 2, 10, Round.QUARTER_FINAL, expectedMatchMap[Pair(3, 2)]!!),
+        TestData(4, 2, 11, Round.QUARTER_FINAL, expectedMatchMap[Pair(3, 2)]!!),
+        TestData(4, 2, 12, Round.QUARTER_FINAL, expectedMatchMap[Pair(3, 2)]!!),
+        TestData(4, 2, 13, Round.QUARTER_FINAL, expectedMatchMap[Pair(4, 2)]!!),
+        TestData(4, 2, 14, Round.QUARTER_FINAL, expectedMatchMap[Pair(4, 2)]!!),
+        TestData(4, 2, 15, Round.QUARTER_FINAL, expectedMatchMap[Pair(4, 2)]!!),
+        TestData(4, 2, 16, Round.QUARTER_FINAL, expectedMatchMap[Pair(4, 2)]!!),
+        TestData(4, 2, 17, Round.ROUND_OF_16, expectedMatchMap[Pair(5, 2)]!!),
+        TestData(4, 2, 18, Round.ROUND_OF_16, expectedMatchMap[Pair(5, 2)]!!),
+        TestData(3, 2, 5, Round.SEMI_FINAL, expectedMatchMap[Pair(2, 2)]!!),
+        TestData(3, 2, 6, Round.SEMI_FINAL, expectedMatchMap[Pair(2, 2)]!!),
+        TestData(3, 2, 7, Round.QUARTER_FINAL, expectedMatchMap[Pair(3, 2)]!!),
+        TestData(3, 2, 9, Round.QUARTER_FINAL, expectedMatchMap[Pair(3, 2)]!!),
+        TestData(3, 2, 12, Round.QUARTER_FINAL, expectedMatchMap[Pair(4, 2)]!!),
+        TestData(3, 2, 14, Round.ROUND_OF_16, expectedMatchMap[Pair(5, 2)]!!),
+        TestData(3, 2, 15, Round.ROUND_OF_16, expectedMatchMap[Pair(5, 2)]!!),
+        TestData(5, 2, 9, Round.SEMI_FINAL, expectedMatchMap[Pair(2, 2)]!!),
+        TestData(5, 2, 10, Round.SEMI_FINAL, expectedMatchMap[Pair(2, 2)]!!),
+        TestData(5, 2, 14, Round.QUARTER_FINAL, expectedMatchMap[Pair(3, 2)]!!),
+        TestData(5, 2, 15, Round.QUARTER_FINAL, expectedMatchMap[Pair(3, 2)]!!),
+        TestData(5, 2, 20, Round.QUARTER_FINAL, expectedMatchMap[Pair(4, 2)]!!),
+        TestData(5, 2, 21, Round.ROUND_OF_16, expectedMatchMap[Pair(5, 2)]!!),
     )
 
     @TestFactory
     fun testPlacementOfSeededRegistrationsWhenTwoAreSeeded() = testDataWhenTwoAreSeeded
         .map { testData ->
-            DynamicTest.dynamicTest("When 2 players per group goes to playoff and number of registrations are ${testData.numberOfRegistrations}") {
+            DynamicTest.dynamicTest(
+                "When pool size is ${testData.playersPerGroup} and ${testData.playersToPlayoff} " +
+                        "players per group goes to playoff and number of registrations are ${testData.numberOfRegistrations}"
+            ) {
                 val competitionCategory = dataGenerator.newCompetitionCategoryDTO(
                     id = 33,
                     settings = dataGenerator.newGeneralSettingsDTO(
                         drawType = DrawType.POOL_AND_CUP,
                         playersPerGroup = testData.playersPerGroup,
-                        playersToPlayOff = 2
+                        playersToPlayOff = testData.playersToPlayoff
                     )
                 )
                 val registrationRanks = generateRegistrationRanks(testData.numberOfRegistrations, competitionCategory)
