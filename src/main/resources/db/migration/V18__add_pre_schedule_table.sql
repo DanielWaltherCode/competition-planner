c59e4a12b8eefc1092f1c2917cdc5f3f4d3c2383
Truls Haraldsson <truls.haraldsson@gmail.com>
2022-04-30 14:48:56 +0200

    Setting NOT NULL constraint on 'success' column

--- src/main/resources/db/migration/V18__add_pre_schedule_table.sql
+++ src/main/resources/db/migration/V18__add_pre_schedule_table.sql
@@ -5,7 +5,7 @@ create table pre_schedule
     time_interval VARCHAR(30) NOT NULL,
     competition_category_id INTEGER references competition_category(id) ON DELETE CASCADE NOT NULL,
     estimated_end_time TIMESTAMP,
-    success BOOLEAN DEFAULT true
+    success BOOLEAN NOT NULL DEFAULT true
 );
 
 CREATE UNIQUE INDEX idx_pre_schedule
