f3f434948ee108ce5d266ee0bb56bc31c1bbefed
Daniel Runvik <daniel.b.walther@gmail.com>
2022-06-08 11:50:42 +0200

    Add new lint file to disable errors

---
+++ web/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        "no-mutating-props": "off"
+    },
+    // These are added if you chose also to install Jest plugin for Vue CLI
+    // With my own modifications here as an example
+    overrides: [
+        {
+            files: [
+                './src/**/__tests__/*.spec.{j,t}s',
+                './src/**/__mock__/*.{j,t}s',
+            ],
+            env: {
+                jest: true,
+            },
+            rules: {
+                'no-unused-expressions': 0,
+            },
+        },
+    ],
+};
