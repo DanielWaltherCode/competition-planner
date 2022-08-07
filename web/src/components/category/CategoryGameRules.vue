fbbb4e937d8bd4662987801554d2974f545c77af
Truls Haraldsson <truls.haraldsson@gmail.com>
2022-08-07 11:31:50 +0200

    Showing error message if user enter an even number of sets.
    When user enter number of sets by text it can happen that number of sets are
    set to an even number.

--- web/src/components/category/CategoryGameRules.vue
+++ web/src/components/category/CategoryGameRules.vue
@@ -5,6 +5,7 @@
         <label class="h5 form-label" for="inputNumberOfSets">{{ $t("categoryGameRules.nrSets") }}</label>
         <input id="inputNumberOfSets" v-model.number="category.gameSettings.numberOfSets" required
                class="form-control" type="number" min="1" step="2">
+        <small v-if="isNumberOfSetsValid" id="numberOfSetsHelp" class="form-text text-muted">Number of sets has to be odd.</small>
       </div>
       <div class="col-md-6 mb-3">
         <label class="h5 form-label" for="inputPlayingUntil">{{ $t("categoryGameRules.winScore") }}</label>
@@ -37,8 +38,9 @@
       </div>
       <div class="col-sm-12 col-md-6 mb-3 d-flex flex-column justify-content-end">
         <label class="h5 form-label" for="inputEndGameNumberOfSets">{{ $t("categoryGameRules.nrSetsEndgame") }}</label>
-        <input id="inputEndGameNumberOfSets" v-model.number="category.gameSettings.numberOfSetsFinal" required
+        <input id="inputEndGameNumberOfSets" v-model.number="category.gameSettings.numberOfSetsFinal"
                class="form-control" type="number" min="1" step="2">
+        <small v-if="isNumberOfSetsInFinalValid" id="numberOfSetsInFinalHelp" class="form-text text-muted">Number of sets has to be odd.</small>
       </div>
       <div class="col-sm-12 mb-5">
         <label class="h5 form-label" for="inputEndGameWinMargin">{{ $t("categoryGameRules.winMarginFinals") }}</label>
@@ -85,6 +87,12 @@ export default {
   computed: {
     competition: function () {
       return this.$store.getters.competition
+    },
+    isNumberOfSetsValid: function () {
+     return (this.category.gameSettings.numberOfSets % 2) === 0
+    },
+    isNumberOfSetsInFinalValid: function() {
+      return (this.category.gameSettings.numberOfSetsFinal % 2) === 0
     }
   },
   mounted() {
