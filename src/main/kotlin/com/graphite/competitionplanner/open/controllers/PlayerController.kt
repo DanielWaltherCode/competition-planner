167a502224724b290b264943576e2f54acab0808
Daniel Runvik <daniel.b.walther@gmail.com>
2022-10-07 19:36:37 +0200

    Add player detail pages
    Both a general player detail and a specific detail page within a given competition.

--- src/main/kotlin/com/graphite/competitionplanner/open/controllers/PlayerController.kt
+++ src/main/kotlin/com/graphite/competitionplanner/open/controllers/PlayerController.kt
@@ -1,22 +1,38 @@
 package com.graphite.competitionplanner.open.controllers
 
+import com.graphite.competitionplanner.competition.domain.FindCompetitions
+import com.graphite.competitionplanner.draw.service.DrawService
 import com.graphite.competitionplanner.player.domain.FindPlayer
 import com.graphite.competitionplanner.player.interfaces.PlayerWithClubDTO
+import com.graphite.competitionplanner.registration.service.RegistrationService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 
 @RequestMapping("open/players")
 @Controller
 class PlayerController(
-        val findPlayer: FindPlayer
+        val findPlayer: FindPlayer,
+        val findCompetitions: FindCompetitions,
+        val registrationService: RegistrationService,
+        val drawService: DrawService
 ) {
 
     @GetMapping
     fun getPlayerOverview(): String {
        return "player/player-search"
+    }
+
+    @GetMapping("/{playerId}")
+    fun getPlayerDetail(model: Model, @PathVariable playerId: Int): String {
+        val player = findPlayer.byId(playerId)
+        val competitions = registrationService.getRegistrationByPlayerId(playerId)
+        model.addAttribute("player", player)
+        model.addAttribute("competitions", competitions)
+        return "player/player-detail"
     }
 
     @GetMapping("/search")
@@ -30,5 +46,23 @@ class PlayerController(
             model.addAttribute("players", emptyList<PlayerWithClubDTO>())
         }
         return "player/player-search-results"
+    }
+
+    @GetMapping("/{competitionId}/{playerId}")
+    fun getPlayerInCompetition(model: Model, @PathVariable competitionId: Int, @PathVariable playerId: Int): String {
+        val player = findPlayer.byId(playerId)
+        val competition = findCompetitions.byId(competitionId)
+        val registrationsInCompetition = registrationService.getRegistrationsForPlayerInCompetition(competitionId, playerId)
+        val playerCategories = registrationsInCompetition.registrations.map { it.competitionCategory.id }
+        val areCategoriesDrawn = mutableMapOf<Int, Boolean>()
+        for (category in playerCategories) {
+            areCategoriesDrawn[category] = drawService.isDrawMade(category)
+        }
+        model.addAttribute("player", player)
+        model.addAttribute("competition", competition)
+        model.addAttribute("registrations", registrationsInCompetition)
+        model.addAttribute("areCategoriesDrawn", areCategoriesDrawn)
+
+        return "competition-detail/player-detail"
     }
 }
