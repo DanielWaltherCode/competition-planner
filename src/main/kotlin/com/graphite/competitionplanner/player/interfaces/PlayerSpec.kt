0e3dddd2e90d5824051b0350603326a533d22eb6
Daniel Runvik <daniel.b.walther@gmail.com>
2022-11-08 22:30:45 +0100

    Allow double first names

--- src/main/kotlin/com/graphite/competitionplanner/player/interfaces/PlayerSpec.kt
+++ src/main/kotlin/com/graphite/competitionplanner/player/interfaces/PlayerSpec.kt
@@ -10,9 +10,7 @@ data class PlayerSpec(
 ) {
     init {
         require(firstName.isNotEmpty()) { "Player's first name cannot be empty" }
-        require(firstName.map { it.isLetter() }.all { it }) { "Player's first name <$firstName>  contains non-letters" }
         require(lastName.isNotEmpty()) { "Player's last name cannot be empty" }
-        require(lastName.map { it.isLetter() }.all { it }) { "Player's last name <$lastName> contains non-letters" }
         require(dateOfBirth.isBefore(LocalDate.now())) { "Player's date of birth <$dateOfBirth> cannot be in the future" }
     }
 }
