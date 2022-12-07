fb6b33c3d1b86f44729bbeb91c510686f1f35ff9
Truls Haraldsson <truls.haraldsson@gmail.com>
2022-12-07 21:57:45 +0100

    TestGenerateTimeTable now setting up own club for tests

--- src/test/kotlin/com/graphite/competitionplanner/schedule/domain/TestGenerateTimeTable.kt
+++ src/test/kotlin/com/graphite/competitionplanner/schedule/domain/TestGenerateTimeTable.kt
@@ -1,31 +1,60 @@
 package com.graphite.competitionplanner.schedule.domain
 
+import com.graphite.competitionplanner.category.interfaces.ICategoryRepository
+import com.graphite.competitionplanner.club.interfaces.IClubRepository
+import com.graphite.competitionplanner.competition.domain.CreateCompetition
 import com.graphite.competitionplanner.competition.domain.FindCompetitions
+import com.graphite.competitionplanner.competition.interfaces.ICompetitionRepository
+import com.graphite.competitionplanner.competitioncategory.interfaces.ICompetitionCategoryRepository
+import com.graphite.competitionplanner.match.repository.MatchRepository
+import com.graphite.competitionplanner.player.interfaces.IPlayerRepository
+import com.graphite.competitionplanner.registration.interfaces.IRegistrationRepository
+import com.graphite.competitionplanner.result.interfaces.IResultRepository
 import com.graphite.competitionplanner.schedule.api.AvailableTablesAllDaysSpec
 import com.graphite.competitionplanner.schedule.api.AvailableTablesSpec
 import com.graphite.competitionplanner.schedule.service.AvailableTablesService
-import com.graphite.competitionplanner.util.TestUtil
+import com.graphite.competitionplanner.util.BaseRepositoryTest
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class TestGenerateTimeTable(
-        @Autowired val timeTableSlotHandler: TimeTableSlotHandler,
-        @Autowired val findCompetitions: FindCompetitions,
-        @Autowired val testUtil: TestUtil,
-        @Autowired val availableTablesService: AvailableTablesService
+    @Autowired val createCompetition: CreateCompetition,
+    @Autowired val timeTableSlotHandler: TimeTableSlotHandler,
+    @Autowired val findCompetitions: FindCompetitions,
+    @Autowired val availableTablesService: AvailableTablesService,
+    @Autowired competitionCategoryRepository: ICompetitionCategoryRepository,
+    @Autowired clubRepository: IClubRepository,
+    @Autowired competitionRepository: ICompetitionRepository,
+    @Autowired categoryRepository: ICategoryRepository,
+    @Autowired playerRepository: IPlayerRepository,
+    @Autowired registrationRepository: IRegistrationRepository,
+    @Autowired matchRepository: MatchRepository,
+    @Autowired resultRepository: IResultRepository
+) : BaseRepositoryTest(
+    clubRepository,
+    competitionRepository,
+    competitionCategoryRepository,
+    categoryRepository,
+    playerRepository,
+    registrationRepository,
+    matchRepository,
+    resultRepository
 ) {
 
     @Test
     fun testGenerateTimeTables() {
         // Set up
-        val clubId = testUtil.getClubIdOrDefault("Lugi")
-        val competition = findCompetitions.thatBelongTo(clubId)[0]
+        val club = newClub()
+        val competition = createCompetition.execute(dataGenerator.newCompetitionSpec(organizingClubId = club.id))
         var tmpDate = competition.startDate
         while (tmpDate < competition.endDate) {
-            availableTablesService.updateTablesAvailable(competition.id, AvailableTablesAllDaysSpec(listOf(AvailableTablesSpec(10, tmpDate))))
-            tmpDate =  tmpDate.plusDays(1)
+            availableTablesService.updateTablesAvailable(
+                competition.id,
+                AvailableTablesAllDaysSpec(listOf(AvailableTablesSpec(10, tmpDate)))
+            )
+            tmpDate = tmpDate.plusDays(1)
         }
 
         // Act
