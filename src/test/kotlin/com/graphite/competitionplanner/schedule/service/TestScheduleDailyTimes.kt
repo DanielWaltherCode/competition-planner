e65318aa1b3ddea5a3a703699784aa4ce51fde39
Truls Haraldsson <truls.haraldsson@gmail.com>
2022-12-08 20:29:47 +0100

    TestSchedule now setup own club before tests

--- src/test/kotlin/com/graphite/competitionplanner/schedule/service/TestScheduleDailyTimes.kt
+++ src/test/kotlin/com/graphite/competitionplanner/schedule/service/TestScheduleDailyTimes.kt
@@ -1,12 +1,22 @@
 package com.graphite.competitionplanner.schedule.service
 
+import com.graphite.competitionplanner.category.interfaces.ICategoryRepository
+import com.graphite.competitionplanner.club.interfaces.IClubRepository
 import com.graphite.competitionplanner.competition.domain.CreateCompetition
 import com.graphite.competitionplanner.competition.interfaces.CompetitionSpec
+import com.graphite.competitionplanner.competition.interfaces.ICompetitionRepository
 import com.graphite.competitionplanner.competition.interfaces.LocationSpec
-import com.graphite.competitionplanner.competition.repository.CompetitionRepository
+import com.graphite.competitionplanner.competitioncategory.interfaces.ICompetitionCategoryRepository
+import com.graphite.competitionplanner.match.repository.MatchRepository
+import com.graphite.competitionplanner.player.interfaces.IPlayerRepository
+import com.graphite.competitionplanner.registration.interfaces.IRegistrationRepository
+import com.graphite.competitionplanner.result.interfaces.IResultRepository
 import com.graphite.competitionplanner.schedule.api.DailyStartAndEndSpec
+import com.graphite.competitionplanner.util.BaseRepositoryTest
 import com.graphite.competitionplanner.util.Util
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import java.time.LocalDate
@@ -14,31 +24,44 @@ import java.time.LocalTime
 
 @SpringBootTest
 class TestScheduleDailyTimes(
-        @Autowired val util: Util,
-        @Autowired val dailyStartEndService: DailyStartEndService,
-        @Autowired val competitionRepository: CompetitionRepository,
-        @Autowired val createCompetition: CreateCompetition
+    @Autowired val util: Util,
+    @Autowired val dailyStartEndService: DailyStartEndService,
+    @Autowired val createCompetition: CreateCompetition,
+    @Autowired clubRepository: IClubRepository,
+    @Autowired competitionRepository: ICompetitionRepository,
+    @Autowired competitionCategoryRepository: ICompetitionCategoryRepository,
+    @Autowired categoryRepository: ICategoryRepository,
+    @Autowired playerRepository: IPlayerRepository,
+    @Autowired registrationRepository: IRegistrationRepository,
+    @Autowired matchRepository: MatchRepository,
+    @Autowired resultRepository: IResultRepository,
+) : BaseRepositoryTest(
+    clubRepository,
+    competitionRepository,
+    competitionCategoryRepository,
+    categoryRepository,
+    playerRepository,
+    registrationRepository,
+    matchRepository,
+    resultRepository
 ) {
+
     var competitionId = 0
 
     @BeforeEach
     fun addCompetition() {
+        val club = newClub()
         competitionId = createCompetition.execute(
                 CompetitionSpec(
                         location = LocationSpec("Lund"),
                         name = "Ein Testturnament",
                         welcomeText = "Välkomna till Eurofinans",
-                        organizingClubId = util.getClubIdOrDefault("Lugi"),
+                        organizingClubId = club.id,
                         competitionLevel = "A",
                         startDate = LocalDate.now(),
                         endDate = LocalDate.now().plusDays(10)
                 )
         ).id
-    }
-
-    @AfterEach
-    fun deleteCompetition() {
-        competitionRepository.deleteCompetition(competitionId)
     }
 
     @Test
