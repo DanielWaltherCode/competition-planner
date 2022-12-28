ceef8d403ae38fd866fb08543e6e00f79ffc16de
Daniel Runvik <daniel.b.walther@gmail.com>
2022-12-28 11:31:55 +0100

    Fix tests
    Also disabled benchmark test until it doesn't clutter the database

--- src/test/kotlin/com/graphite/competitionplanner/billing/domain/TestGetCostSummaryForClub.kt
+++ src/test/kotlin/com/graphite/competitionplanner/billing/domain/TestGetCostSummaryForClub.kt
@@ -2,6 +2,7 @@ package com.graphite.competitionplanner.billing.domain
 
 import com.graphite.competitionplanner.club.repository.ClubRepository
 import com.graphite.competitionplanner.competition.domain.FindCompetitions
+import com.graphite.competitionplanner.util.TestUtil
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,15 +13,17 @@ import java.time.LocalDate
 class TestGetCostSummaryForClub(
     @Autowired val findCompetitions: FindCompetitions,
     @Autowired val clubRepository: ClubRepository,
-    @Autowired val getCostSummaryForClub: GetCostSummaryForClub
+    @Autowired val getCostSummaryForClub: GetCostSummaryForClub,
+    @Autowired val util: TestUtil
 ) {
 
     @Test
     fun getCostSummaryForClub() {
-        val competitions = findCompetitions.thatStartOrEndWithin(LocalDate.now().minusYears(2), LocalDate.now())
-        val firstCompetitionId = competitions[0].id
-        val clubsInCompetition = clubRepository.getClubsInCompetition(firstCompetitionId)
-        val costSummary = getCostSummaryForClub.execute(firstCompetitionId, clubsInCompetition[0].id)
+        val lugiId = util.getClubIdOrDefault("Lugi")
+        val lugiCompetitions = findCompetitions.thatBelongTo(lugiId)
+        val lugiCompetitionId = lugiCompetitions[0].id
+        val clubsInCompetition = clubRepository.getClubsInCompetition(lugiCompetitionId)
+        val costSummary = getCostSummaryForClub.execute(lugiCompetitionId, clubsInCompetition[0].id)
         Assertions.assertNotNull(costSummary.club)
         Assertions.assertTrue(costSummary.costSummaries.isNotEmpty())
     }
--- src/test/kotlin/com/graphite/competitionplanner/billing/domain/TestGetCostSummaryForPlayers.kt
+++ src/test/kotlin/com/graphite/competitionplanner/billing/domain/TestGetCostSummaryForPlayers.kt
@@ -2,6 +2,7 @@ package com.graphite.competitionplanner.billing.domain
 
 import com.graphite.competitionplanner.club.repository.ClubRepository
 import com.graphite.competitionplanner.competition.domain.FindCompetitions
+import com.graphite.competitionplanner.util.TestUtil
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,17 +11,19 @@ import java.time.LocalDate
 
 @SpringBootTest
 class TestGetCostSummaryForPlayers(
-    @Autowired val findCompetitions: FindCompetitions,
-    @Autowired val clubRepository: ClubRepository,
-    @Autowired val getCostSummaryForPlayers: GetCostSummaryForPlayers,
+        @Autowired val findCompetitions: FindCompetitions,
+        @Autowired val clubRepository: ClubRepository,
+        @Autowired val getCostSummaryForPlayers: GetCostSummaryForPlayers,
+        @Autowired val util: TestUtil
 ) {
 
     @Test
     fun getCostSummaryForPlayers() {
-        val competitions = findCompetitions.thatStartOrEndWithin(LocalDate.now().minusYears(2), LocalDate.now())
-        val firstCompetitionId = competitions[0].id
-        val clubsInCompetition = clubRepository.getClubsInCompetition(firstCompetitionId)
-        val costSummary = getCostSummaryForPlayers.execute(firstCompetitionId, clubsInCompetition[0].id)
+        val lugiId = util.getClubIdOrDefault("Lugi")
+        val lugiCompetitions = findCompetitions.thatBelongTo(lugiId)
+        val lugiCompetitionId = lugiCompetitions[0].id
+        val clubsInCompetition = clubRepository.getClubsInCompetition(lugiCompetitionId)
+        val costSummary = getCostSummaryForPlayers.execute(lugiCompetitionId, clubsInCompetition[0].id)
         Assertions.assertTrue(costSummary.costSummaryList.isNotEmpty())
         Assertions.assertTrue(costSummary.totalPrice > 0)
     }
